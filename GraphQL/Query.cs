c2bf5af7a8285d06762f265256cffd42a88627e1
sailakshmy <sailakshmy94@gmail.com>
2023-10-16 21:54:16 +0530

    Commit-9:- Query Arguments

--- GraphQL/Query.cs
+++ GraphQL/Query.cs
@@ -3,14 +3,20 @@ using System.Text.Json.Serialization;
 
 public class Query
 {
-    public List<Book> Books => ReadBooks();   
+    // public List<Book> Books => ReadBooks();   
     public List<Magazine> Magazines => ReadMagazines();
     public List<MyReadingMaterials> ReadingMaterials => GetReadingMaterials();
     public List<IThings> Things => GetThings();
-    private List<Book> ReadBooks()  {
+    // private List<Book> ReadBooks()  {
+    //     string fileName = "Database/books.json";
+    //     string jsonString = File.ReadAllText(fileName);
+    //     return JsonSerializer.Deserialize<List<Book>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters={new JsonStringEnumConverter()} })!;
+    // }
+    public List<Book> Books(string nameContains="")  {
         string fileName = "Database/books.json";
         string jsonString = File.ReadAllText(fileName);
-        return JsonSerializer.Deserialize<List<Book>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters={new JsonStringEnumConverter()} })!;
+        var books= JsonSerializer.Deserialize<List<Book>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters={new JsonStringEnumConverter()} })!;
+        return books.Where(book=>book.Name.IndexOf(nameContains)!=-1).ToList();
     }
     private List<Magazine> ReadMagazines()  {
         string fileName = "Database/magazines.json";
@@ -18,12 +24,12 @@ public class Query
         return JsonSerializer.Deserialize<List<Magazine>>(jsonString, new JsonSerializerOptions { PropertyNameCaseInsensitive = true, Converters={new JsonStringEnumConverter()} })!;
     }
     private List<MyReadingMaterials> GetReadingMaterials(){
-        var materials = ReadBooks().Cast<MyReadingMaterials>().ToList();
+        var materials = Books().Cast<MyReadingMaterials>().ToList();
         materials.AddRange(ReadMagazines().Cast<MyReadingMaterials>().ToList());
         return materials;
     }
     private List<IThings> GetThings(){
-        var things = ReadBooks().Cast<IThings>().ToList();
+        var things = Books().Cast<IThings>().ToList();
         things.AddRange(ReadMagazines().Cast<IThings>().ToList());
         return things;
     }

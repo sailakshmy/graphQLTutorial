4f7d1ba5bec80845cab8f039ea7513656611e42b
sailakshmy <sailakshmy94@gmail.com>
2023-10-16 11:42:17 +0530

    Commit-2:- Added new fields in Book.cs

--- Models/Book.cs
+++ Models/Book.cs
@@ -2,4 +2,6 @@ public class Book  {
     public int BookId { get; set; }
     public string Name { get; set; }
     public int Pages { get; set; }
+    public double Price {get; set;}
+    public DateTime PublishedDate {get; set;}
 }

8773752f9e1df9b5c23f0a4a4bac4826819a74ab
sailakshmy <sailakshmy94@gmail.com>
2023-10-17 18:14:43 +0530

    Commit-10:- Mutations- Input Types

---
+++ Models/BookInput.cs
@@ -0,0 +1,8 @@
+public class BookInput {
+    public string NewBookName {get;set;}
+    public int NewBookPages { get; set; }
+    public double NewBookPrice {get; set;}
+    public DateTime? NewBookPublishedDate {get; set;}
+    public BookGenre NewBookGenre{get;set;} 
+
+}
